87d4d791416100d5f110a4a62a0313cf893e39b0
Kaya <kayaopinternet@protonmail.com>
2023-04-19 19:44:10 +0200

    Implemented extracting image embeddings from the SAM image encoder

--- extract_sam_features.py
+++ extract_sam_features.py
@@ -12,11 +12,13 @@ train_data/
     imgN.tar
 """
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+from segment_anything.utils.transforms import ResizeLongestSide
 
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 from torchvision.datasets import CocoDetection
+import torch
 
 
 def parse_args():
@@ -57,6 +59,28 @@ def show_anns(anns):
         ax.imshow(np.dstack((img, m * 0.35)))
 
 
+def extract_image_embedding(model, image, transform):
+    """Extract the image embedding from the model encoder."""
+    # Transform the image to the form expected by the model
+    input_image = transform.apply_image(image)
+    input_image = torch.as_tensor(input_image, device=model.device)
+    input_image = input_image.permute(2, 0, 1).contiguous()[None, :, :, :]
+
+    # Preprocess the image (e.g. normalization)
+    input_image = model.preprocess(input_image)
+
+    # Obtain the image features.
+    img_embed = model.image_encoder(input_image)
+
+    return img_embed
+
+
+def extract_and_save_features(mask_generator, image, save_loc):
+    """Extract the features for all masks found in the image and save them."""
+    masks = mask_generator.generate(image)
+    mask_feature = {"mask": masks["segmentation"], "feature": masks["mask_feature"]}
+
+
 if __name__ == "__main__":
     args = parse_args()
     device = "cuda" if args.gpu else "cpu"
@@ -66,12 +90,16 @@ if __name__ == "__main__":
     sam.to(device)
     mask_generator = SamAutomaticMaskGenerator(sam)
 
-    for img, _ in dataset:
-        masks = mask_generator.generate(np.array(img))
-        print(len(masks))
-        print(masks[0].keys())
+    transform = ResizeLongestSide(sam.image_encoder.img_size)
 
-        print(masks[0]["mask_feature"].shape)
+    for img, _ in dataset:
+        # masks = mask_generator.generate(np.array(img))
+        # print(len(masks))
+        # print(masks[0].keys())
+
+        # print(masks[0]["mask_feature"].shape)
+        img_embed = extract_image_embedding(sam, np.array(img), transform)
+        print(img_embed.shape)
 
         # plt.figure(figsize=(20, 20))
         # plt.imshow(img)
