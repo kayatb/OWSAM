e74f67398f3103a8f1125b5f4d4d0b47a459c746
Kaya <kayaopinternet@protonmail.com>
2023-04-26 13:57:43 +0200

    Added COCO GT annotations to the embedding dataset

--- data/img_embeds_dataset.py
+++ data/img_embeds_dataset.py
@@ -1,19 +1,17 @@
-from segment_anything.utils.amg import build_all_layer_point_grids
-
 import torch
 import os
+from pycocotools.coco import COCO
 
 
 class ImageEmbeds(torch.utils.data.Dataset):
     """Load the pre-extracted image embeddings into a torch Dataset."""
 
-    def __init__(self, dir, device, points_per_side=32):
+    def __init__(self, dir, ann_file, device):
         """Load the image embeddings from `dir`."""
         self.dir = dir
         self.files = os.listdir(dir)
+        self.coco = COCO(ann_file)
         self.device = device
-
-        self.point_grids = build_all_layer_point_grids(points_per_side, 0, 1)
 
     def __getitem__(self, idx):
         """Returns the image embedding (256 x 64 x 64), the original image size (W x H), the image file name,
@@ -21,10 +19,15 @@ class ImageEmbeds(torch.utils.data.Dataset):
         file_path = os.path.join(self.dir, self.files[idx])
         img_data = torch.load(file_path, map_location=self.device)
 
+        img_id = int(os.path.splitext(self.files[idx])[0])
+        ann_ids = self.coco.getAnnIds(imgIds=[img_id], iscrowd=None)
+        anns = self.coco.loadAnns(ann_ids)
+
         return {
             "embed": img_data["embed"],
             "original_size": img_data["orig_size"],
-            "file_name": os.path.splitext(self.files[idx])[0],
+            "gt_anns": anns,
+            "img_id": img_id,
         }
 
     def __len__(self):
@@ -34,16 +37,17 @@ class ImageEmbeds(torch.utils.data.Dataset):
     def collate_fn(data):
         embeds = torch.stack([d["embed"] for d in data])
         original_sizes = [d["original_size"] for d in data]
-        file_names = [d["file_name"] for d in data]
+        img_ids = [d["img_id"] for d in data]
+        anns = [d["gt_anns"] for d in data]
 
-        return {"embed": embeds, "original_size": original_sizes, "file_name": file_names}
+        return {"embed": embeds, "original_size": original_sizes, "img_id": img_ids, "gt_anns": anns}
 
 
 if __name__ == "__main__":
-    dataset = ImageEmbeds("img_embeds", "cpu")
+    dataset = ImageEmbeds("img_embeds", "../datasets/coco/annotations/instances_val2017.json", "cpu")
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, collate_fn=ImageEmbeds.collate_fn)
 
     for batch in dataloader:
         print(batch["embed"].shape)
         print(batch["original_size"])
-        print(batch["file_name"])
+        print(len(batch["gt_anns"]))
