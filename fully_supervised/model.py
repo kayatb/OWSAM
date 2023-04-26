e29f422554280221688d2577b3622b324101cd40
Kaya <kayaopinternet@protonmail.com>
2023-04-26 18:05:42 +0200

    Added padding to classification logits and bboxes to be able to stack in a single tensor

--- fully_supervised/model.py
+++ fully_supervised/model.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
 
-# import torch.nn.functional as F
+import torch.nn.functional as F
 
 
 class FullySupervisedClassifier(nn.Module):
-    def __init__(self, sam_generator, num_layers, hidden_dim, num_classes, input_dim=256):
+    def __init__(self, sam_generator, num_layers, hidden_dim, num_classes, pad_num=200, input_dim=256):
         super().__init__()
 
         self.num_classes = num_classes + 1  # Account for the backrground/no-object class.
+        self.pad_num = pad_num
 
         self.sam_generator = sam_generator
 
@@ -23,14 +24,18 @@ class FullySupervisedClassifier(nn.Module):
 
     # TODO: batch-ify this
     def forward(self, batch):
+        batch_size = batch["embed"].shape[0]
         masks, boxes, mask_features = self.get_mask_features(batch)
 
-        class_logits = []
-        for feature in mask_features:
+        class_logits = torch.zeros((batch_size, self.pad_num, self.num_classes))
+        for i, feature in enumerate(mask_features):
             x = self.layers(feature)
             x = self.classifier(x)
-            class_logits.append(x)
+            # Pad the logits to be able to stack them in a single tensor.
+            # Necessary since the amount of found masks is not constant across images.
+            class_logits[i] = F.pad(x, (0, 0, 0, self.pad_num - x.shape[0]), "constant", -1000)
 
+        # F.pad(t, (0, 0, 0, 20), "constant", 0)
         return {
             "masks": masks,
             "pred_logits": class_logits,
@@ -38,12 +43,13 @@ class FullySupervisedClassifier(nn.Module):
         }  # TODO: could also return mask_features here, but don't think it's necessary
 
     def get_mask_features(self, batch):
+        batch_size = batch["embed"].shape[0]
         masks = []
-        boxes = []
+        boxes = torch.zeros((batch_size, self.pad_num, 4))
         mask_features = []
 
         # TODO: batch-ify this.
-        for i in range(batch["embed"].shape[0]):
+        for i in range(batch_size):
             batch_masks = []
             batch_bbox = []
             batch_mask_features = []
@@ -56,7 +62,10 @@ class FullySupervisedClassifier(nn.Module):
                 batch_mask_features.append(torch.as_tensor(mask["mask_feature"]))
 
             masks.append(torch.stack(batch_masks))
-            boxes.append(torch.stack(batch_bbox))
+            batch_bbox = torch.stack(batch_bbox)
+            # Pad the bboxes to be able to stack them in a single tensor.
+            # Necessary since the amount of found masks is not constant across images.
+            boxes[i] = F.pad(batch_bbox, (0, 0, 0, self.pad_num - batch_bbox.shape[0]), "constant", 0)
             mask_features.append(torch.stack(batch_mask_features))
 
         return masks, boxes, mask_features
@@ -84,8 +93,10 @@ if __name__ == "__main__":
         output = model(batch)
 
         print(len(output["pred_boxes"]))
-        for box in output["pred_boxes"]:
-            print(box.shape)
+        print(output["pred_boxes"].shape)
+
+        print(output["pred_logits"].shape)
+
     # for a in output:
     #     print(a.shape)
     # print(model(batch).shape)
