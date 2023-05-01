989fb08198d9fb3a4d06d0263f1e3c1a70ce904d
Kaya <kayaopinternet@protonmail.com>
2023-05-01 12:08:07 +0200

    Extract SAM masks and features and save as gzip

--- data/extraction/extract_sam_masks.py
+++ data/extraction/extract_sam_masks.py
@@ -6,8 +6,11 @@ from segment_anything import sam_model_registry
 import argparse
 import os
 import io
-import tarfile
-import numpy as np
+
+# import tarfile
+import gzip
+
+# import numpy as np
 from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
@@ -17,12 +20,6 @@ def parse_args():
     parser = argparse.ArgumentParser(
         prog="SAMFeatureExtractor",
         description="Extract mask features from SAM for given COCO dataset",
-    )
-    parser.add_argument(
-        "-i",
-        "--image-dir",
-        required=True,
-        help="Dir of the dataset images",
     )
     parser.add_argument("-e", "--embed-dir", required=True, help="Dir of the pre-extracted image embeddings")
     parser.add_argument(
@@ -41,25 +38,20 @@ def save_all_masks(mask_generator, dataloader, save_dir):
     os.makedirs(save_dir, exist_ok=True)
 
     for batch in tqdm(dataloader):
-        # {"embed": embeds, "original_size": original_sizes, "img_id": img_ids, "targets": targets}
+        # batch contains {"embed": embeds, "original_size": original_sizes, "img_id": img_ids}
         batch_size = batch["embed"].shape[0]
         for i in range(batch_size):
             output = mask_generator.generate(batch["embed"][i].unsqueeze(0), batch["original_size"][i])
             # output is a list of dicts (one for each mask generated for that image), with keys:
-            # 'segmentation', 'area', 'bbox', 'predicted_iou', 'point_coords', 'stability_score', 'mask_feature']
+            # 'segmentation', 'area', 'bbox', 'predicted_iou', 'point_coords', 'stability_score', 'mask_feature'
             # For each image, save a tar file with all the masks. The tar file has the img_id as filename.
             # with tarfile.open(f"{batch["img_id"]}.tar.gz", "w:gz") as fp:
-            # for j, mask in enumerate(output):
             buffer = io.BytesIO()
             torch.save(output, buffer)
-            with tarfile.open(os.path.join(save_dir, f"{batch['img_id'][i]}.tar.gz"), "w:gz") as fp:
-                buffer.seek(0)
-                tarinfo = tarfile.TarInfo(name=f"{batch['img_id'][i]}")
-                tarinfo.size = len(buffer.getvalue())
-                fp.addfile(tarinfo=tarinfo, fileobj=buffer)
 
-            # print(len(masks))
-            # print(masks[0].keys())
+            with gzip.open(os.path.join(save_dir, f"{batch['img_id'][i]}.gz"), "wb") as fp:
+                buffer.seek(0)
+                fp.write(buffer.read())
 
 
 if __name__ == "__main__":
@@ -68,15 +60,16 @@ if __name__ == "__main__":
 
     sam = sam_model_registry["vit_h"](checkpoint="checkpoints/sam_vit_h_4b8939.pth")
 
-    dataset = ImageEmbeds(args.embed_dir, args.image_dir, device)
+    dataset = ImageEmbeds(args.embed_dir, device)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, collate_fn=ImageEmbeds.collate_fn)
 
     mask_generator = OWSamMaskGenerator(sam)
     sam.to(device=device)  # Should be done after the decoder has been changed by the mask generator
 
     save_all_masks(mask_generator, dataloader, args.save_dir)
-    # with tarfile.open("mask_features/459195.tar.gz", "r") as fp:
-    #     for member in fp:
-    #         # print(member)
-    #         f = fp.extractfile(member)
-    #         print(torch.load(f))
+
+    # with gzip.open("mask_features/459195.gz", "rb") as fp:
+    #     content = fp.read()
+    # t = torch.load(io.BytesIO(content))
+    # print(len(t))
+    # print(t[0].keys())
