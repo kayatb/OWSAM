93f4f219442cd0304c171808a75ef66e78169e42
Kaya <kayaopinternet@protonmail.com>
2023-05-12 13:20:13 +0200

    Added extension of mask feature datasets with bbox crops

--- data/datasets/mask_feature_dataset.py
+++ data/datasets/mask_feature_dataset.py
@@ -1,8 +1,10 @@
-from utils.misc import filter_empty_imgs
+from utils.misc import filter_empty_imgs, crop_bboxes_from_img, box_xywh_to_xyxy
 
 import torch
+import torchvision.transforms as T
 import os
 from pycocotools.coco import COCO
+from PIL import Image
 
 
 class MaskData(torch.utils.data.Dataset):
@@ -32,7 +34,6 @@ class MaskData(torch.utils.data.Dataset):
         """Returns the masks, boxes, mask_features, iou_scores, the image id (i.e. filename),
         and the targets (class labels and segmentation masks) from the COCO dataset."""
         file_path = os.path.join(self.dir, self.files[idx])
-        # mask_data = self.get_mask_data(file_path)
         # mask_data is a list of dicts (one dict per predicted mask in the image), where each dict has the following
         # keys: 'area', 'bbox', 'predicted_iou', 'stability_score', 'mask_feature'
         mask_data = torch.load(file_path, map_location=self.device)
@@ -55,7 +56,6 @@ class MaskData(torch.utils.data.Dataset):
             iou_scores[i] = mask["predicted_iou"]
 
         return {
-            # "masks": [mask["segmentation"] for mask in mask_data],
             "boxes": boxes,
             "mask_features": mask_features,
             "iou_scores": iou_scores,
@@ -112,33 +112,79 @@ class MaskData(torch.utils.data.Dataset):
         }
 
 
+class CropMaskData(MaskData):
+    def __init__(self, box_dir, ann_file, img_dir, device, pad_num=700, resize=128):
+        super().__init__(box_dir, ann_file, device, pad_num)
+
+        self.img_dir = img_dir
+        self.resize = resize
+
+    def __getitem__(self, idx):
+        data = super().__getitem__(idx)
+
+        boxes = box_xywh_to_xyxy(data["boxes"][: data["num_masks"]])  # Convert format and remove padding.
+
+        # COCO image files have filename img_id prepended with 0's until length is 12.
+        img_file = f"{str(data['img_id']).rjust(12, '0')}.jpg"
+        with Image.open(os.path.join(self.img_dir, img_file)) as img:  # TODO: how to get filename of image file?
+            img = img.convert("RGB")
+            crops = crop_bboxes_from_img(img, boxes)
+
+        for i, crop in enumerate(crops):
+            crops[i] = self.preprocess(crop)
+
+        data["crops"] = torch.stack(crops)
+
+        return data
+
+    def preprocess(self, img):
+        transform = T.Compose(
+            [
+                T.Resize(self.resize),
+                T.CenterCrop(self.resize),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        img = transform(img)[:3]  # .unsqueeze(0)
+
+        return img
+
+    @staticmethod
+    def collate_fn(data):
+        batch = MaskData.collate_fn(data)
+        batch["crops"] = [d["crops"] for d in data]
+
+        return batch
+
+
 if __name__ == "__main__":
     from tqdm import tqdm
 
-    dataset = MaskData("mask_features/train_all", "../datasets/coco/annotations/instances_train2017.json", "cpu")
+    # dataset = MaskData("mask_features/train_all", "../datasets/coco/annotations/instances_train2017.json", "cpu")
 
-    dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, collate_fn=MaskData.collate_fn, num_workers=12, pin_memory=True, persistent_workers=True
+    # dataloader = torch.utils.data.DataLoader(
+    #     dataset, batch_size=1, collate_fn=MaskData.collate_fn, num_workers=12, pin_memory=True, persistent_workers=True
+    # )
+
+    # # Calculate the maximum amount of masks detected by SAM.
+    # # 666 for train set, 395 for val set.
+    # max_dim = 0
+    # for batch in tqdm(dataloader):
+    #     max_dim = max(max_dim, batch["mask_features"].shape[0])
+    # print(max_dim)
+
+    dataset = CropMaskData(
+        "mask_features/train_all",
+        "../datasets/coco/annotations/instances_train2017.json",
+        "../datasets/coco/train2017",
+        "cpu",
     )
-
-    # Calculate the maximum amount of masks detected by SAM.
-    # 666 for train set, 395 for val set.
-    max_dim = 0
-    for batch in tqdm(dataloader):
-        max_dim = max(max_dim, batch["mask_features"].shape[0])
-    print(max_dim)
-
-    # for file in tqdm(dataset.files):
-    #     path = os.path.join(dataset.dir, file)
-    #     with gzip.open(path, "rb") as fp:
-    #         fp.read()
-    # mask_data = torch.load(io.BytesIO(fp.read()), map_location=dataset.device)
-
-    # for batch in dataloader:
-    #     print(len(batch["masks"]))
-    #     print(batch["mask_features"].shape)
-    #     print(batch["boxes"].shape)
-    #     print(batch["iou_scores"].shape)
-    #     print(batch["num_masks"])
-    #     print(batch["img_ids"])
-    #     print(batch["targets"])
+    # print(dataset[0]["crops"].shape)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=3, collate_fn=CropMaskData.collate_fn)
+    for batch in dataloader:
+        print(batch.keys())
+        for crop in batch["crops"]:
+            print(crop.shape)
+        break
